0de87e58b2bc77e9b3b37df2b1965717e37ab251
emhl <emil@steinr.eu>
2024-01-16 14:57:20 +0100

    schrotrauschen direkt und ttest part 3

--- fp.py
+++ fp.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.integrate import quad
 
 # normalize
 def normalize(arr, t_min, t_max):    
@@ -38,3 +39,42 @@ def norm2(arr):
 def mapcolors(n):
     """Return a list of n colors from the default colormap"""
     return [plt.cm.jet(int(x*plt.cm.jet.N/n)) for x in range(n)]
+
+# signifikanztest
+
+def ttest(werte,unsicherheiten):
+    for i,(v1, uv1) in enumerate(zip(werte,unsicherheiten)):
+        # print(i,end = '\t')
+        for j,(v2, uv2) in enumerate(zip(werte, unsicherheiten)):
+            d = abs(v1-v2)
+            ud = np.sqrt(uv1**2 + uv2**2)
+            t = d/ud
+            print(round(t,3), end = '\t')
+        print()
+
+def prob(x):
+    return np.exp(-x**2/2)/np.sqrt(2*np.pi)
+
+
+def ttestPaussen(werte,unsicherheiten):
+    for i,(v1, uv1) in enumerate(zip(werte,unsicherheiten)):
+        for j,(v2, uv2) in enumerate(zip(werte, unsicherheiten)):
+            d = abs(v1-v2)
+            ud = np.sqrt(uv1**2 + uv2**2)
+            t = d/ud
+            P = quad(prob,-t,t)
+            # print(round((P[0])*100,3), end = '\t')
+            print(f"\SI{{{round((P[0])*100,3)}}}" + r"{\percent}", end = '\t')
+            # print(P, end = '\t')
+        print()
+def ttestPinnen(werte,unsicherheiten):
+    for i,(v1, uv1) in enumerate(zip(werte,unsicherheiten)):
+        for j,(v2, uv2) in enumerate(zip(werte, unsicherheiten)):
+            d = abs(v1-v2)
+            ud = np.sqrt(uv1**2 + uv2**2)
+            t = d/ud
+            P = quad(prob,-t,t)
+            # print(round((1-P[0])*100,3), end = '\t')
+            print(f"\SI{{{round((1-P[0])*100,3)}}}" + r"{\percent}", end = '\t')
+            # print(P, end = '\t')
+        print()
